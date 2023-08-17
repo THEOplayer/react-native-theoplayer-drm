755fc817531821f38435226800b3fc5ed976ff26
Ivar Vermeulen <github@ivarvermeulen.nl>
2023-08-17 13:49:35 +0200

    feat(castlabs): make sure widevine licenseAcquisitionURL doesn't have specConform url param, part 2

--- src/castlabs/CastLabsWidevineContentProtectionIntegration.ts
+++ src/castlabs/CastLabsWidevineContentProtectionIntegration.ts
@@ -1,6 +1,6 @@
-import type { ContentProtectionIntegration, LicenseRequest, MaybeAsync } from 'react-native-theoplayer';
+import type { ContentProtectionIntegration, LicenseRequest, LicenseResponse, MaybeAsync } from 'react-native-theoplayer';
 import type { CastLabsDrmConfiguration } from './CastLabsDrmConfiguration';
-import { fromObjectToBase64String } from 'react-native-theoplayer';
+import { fromObjectToBase64String, fromUint8ArrayToString, fromBase64StringToUint8Array } from 'react-native-theoplayer';
 
 export class CastLabsWidevineContentProtectionIntegration implements ContentProtectionIntegration {
   static readonly DEFAULT_LICENSE_URL = 'https://lic.drmtoday.com/license-proxy-widevine/cenc/';
@@ -19,15 +19,20 @@ export class CastLabsWidevineContentProtectionIntegration implements ContentProt
   }
 
   onLicenseRequest(request: LicenseRequest): MaybeAsync<Partial<LicenseRequest> | BufferSource> {
-    // DRMToday has the option to return licences as binary data by using ?specConform=true, however we expect JSON so remove url params from licenseUrl
-    let licenseUrl = new URL(this.contentProtectionConfiguration.widevine?.licenseAcquisitionURL ?? CastLabsWidevineContentProtectionIntegration.DEFAULT_LICENSE_URL);
-    request.url = licenseUrl.protocol + "//" + licenseUrl.host + licenseUrl.pathname;
-
+    request.url = (this.contentProtectionConfiguration.widevine?.licenseAcquisitionURL ?? CastLabsWidevineContentProtectionIntegration.DEFAULT_LICENSE_URL).replace("?specConform=true","");
     request.headers = {
       ...request.headers,
       'x-dt-custom-data': this.customData!,
       'x-dt-auth-token': this.contentProtectionConfiguration.integrationParameters.token ?? '',
     };
+
     return request;
+  }
+
+  onLicenseResponse?(response: LicenseResponse): MaybeAsync<BufferSource> {
+    const responseAsText = fromUint8ArrayToString(response.body);
+    const responseObject = JSON.parse(responseAsText);
+
+    return fromBase64StringToUint8Array(responseObject.license);
   }
 }
